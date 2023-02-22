a802519ef51646674ff8238f418731652c811ee8
Francesco Consiglio <francescoconsiglio98@gmail.com>
2023-02-22 15:31:48 +0100

    Initial commit: exercise completed

---
+++ src/main/java/co/develhope/websocket1/controller/NotificationController.java
@@ -0,0 +1,27 @@
+package co.develhope.websocket1.controller;
+
+import co.develhope.websocket1.entities.MessageDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("broadcast-message")
+public class NotificationController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+
+    @PostMapping
+    public ResponseEntity<HttpStatus> sendNotification(@RequestBody MessageDTO messageDTO) {
+        simpMessagingTemplate.convertAndSend("/topic/broadcast", messageDTO);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+}
---
+++ src/main/java/co/develhope/websocket1/entities/MessageDTO.java
@@ -0,0 +1,23 @@
+package co.develhope.websocket1.entities;
+
+public class MessageDTO {
+
+    private String type;
+    private String message;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
